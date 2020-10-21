2b3e72204cc66c098dcb555220c5f6e9df64d337
coovi <coovi.meha@colorado.edu>
2020-10-20 23:46:00 -0600

    start partb

--- Coovi_Meha_HW3/hw3.py
+++ Coovi_Meha_HW3/hw3.py
@@ -10,46 +10,63 @@ def test_exo2_a():
     start = [0, 0]
     goal = [10, 0]
     length = 1
+    fig = plt.figure(1, figsize=plt.figaspect(2.))
     exo2 = exercise2(start, goal, center, length)
     obs = exo2.make_obs()
     q, pot_fun, u_atts, u_reps = exo2.compute()
-    # print(u_reps[100])
-    # print(np.linalg.norm(pot_fun))
-    plt.scatter([0, 10], [0, 0])
-    plt.xlim([-1, 11])
-    plt.ylim([-2, 5])
-    plt.plot_
+
+    ax = fig.add_subplot(2, 1, 1)
+    ax.scatter([0, 10], [0, 0])
+    ax.set_xlim([-1, 11])
+    ax.set_ylim([-2, 5])
     angles = np.linspace(0, 2*pi, 100)
     bond_obs = [np.transpose(0.25*np.cos(angles) + goal[0]),
                 np.transpose(0.25*np.sin(angles) + goal[1])]
-    plt.plot(bond_obs[0], bond_obs[1])
+    ax.plot(bond_obs[0], bond_obs[1])
     for i in range(0, len(obs)):
-        plt.plot(obs[i][0], obs[i][1])
+        ax.plot(obs[i][0], obs[i][1])
     iters = 10
     for i in range(iters, len(q[0])-iters, iters):
-        plt.plot([q[0][i], q[0][i+iters]], [q[1][i], q[1][i+iters]])
+        ax.plot([q[0][i], q[0][i+iters]], [q[1][i], q[1][i+iters]])
         plt.pause(0.01)
+
+    # -------------------------------------------------------------- potential fieled
+    # ----------------------------------------------------------------------
+    x, y, z = exo2.potenfield_generator()
+    ax = fig.add_subplot(2, 1, 2, projection='3d')
+    X, Y = np.meshgrid(x, y)
+    color_map = plt.cm.get_cmap('jet_r')
+    ax.plot_surface(X, Y, z,
+                    cmap=color_map, edgecolor='none')
     plt.show()
 
 
 def test_exo2_b():
-    center = [[4, 1], [7, -1]]
+    w1 = [[(1,  1), (2,  1), (2,  5), (1,  5)],
+          [(3,  4), (4,  4), (4,  12), (3,  12)],
+          [(3,  12), (12,  12), (12,  13), (3,  13)],
+          [(12,  5), (13,  5), (13,  13), (12,  13)],
+          [(6,  5), (12,  5), (12,  6), (6,  6)]]
+    w2 = [
+        [(-6,  -6), (25,  -6), (25,  -5), (-6,  -5)],
+        [(-6,  5), (30,  5), (30,  6), (-6,  6)],
+        [(-6,  -5), (-5,  -5), (-5,  5), (-6,  5)],
+        [(4,  -5), (5,  -5), (5,  1), (4,  1)],
+        [(9,  0), (10,  0), (10,  5), (9,  5)],
+        [(14,  -5), (15,  -5), (15,  1), (14,  1)],
+        [(19,  0), (20,  0), (20,  5), (19,  5)],
+        [(24,  -5), (25,  -5), (25,  1), (24,  1)],
+        [(29,  0), (30,  0), (30,  5), (29,  5)]]
+    center = w1
     start = [0, 0]
-    goal = [10, 0]
+    goal = [10, 10]
     length = 1
-    exo2 = exercise2(start, goal, center, length)
-    obs = exo2.make_obs()
+    fig = plt.figure(1, figsize=plt.figaspect(4.))
+    exo2 = exercise2(start, goal, center, length,True)
     q, pot_fun, u_atts, u_reps = exo2.compute()
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    upot = (u_atts+u_reps)
-    print(len(q[0]))
-    n = int(np.size(upot)/2)
-    z = np.reshape(upot, (n, n))
-
-    ax.plot_surface(q[0], q[1], z,
-                    cmap='viridis', edgecolor='none')
-
-
+    ax = fig.add_subplot(2, 1, 1)
+    ax.scatter([0, 10], [0, 10])
+    w1_point=zip(*w1)
+    ax.plot(w1_point[0], w1_point[1])
 if __name__ == "__main__":
     test_exo2_b()
--- Coovi_Meha_HW3/src/exercise2.py
+++ Coovi_Meha_HW3/src/exercise2.py
@@ -5,13 +5,14 @@ from numpy import array
 
 
 class exercise2:
-    def __init__(self, start, goal, center, length):
+    def __init__(self, start, goal, center, length, is_coord=False):
         self.center = center
         self.length = length
         self.goal = goal
         self.start = start
-        self.radius = ((2*self.length**2)**.5)/2
+        self.radius = [((2*self.length**2)**.5)/2, ((2*self.length**2)**.5)/2]
         self.epsi = 0.1
+        self.is_coord = is_coord
 
     def potential_func(self):
         pot_fun = float('inf')
@@ -53,7 +54,6 @@ class exercise2:
                     pot_repu_i = n*num*grad*d_sq
 
                     rep = .5*n*(1/distance_from_obs_edge[i] - 1/d_q_start)**2
-                    # print(i)
                 else:
                     pot_repu_i = 0
                     rep = 0
@@ -62,7 +62,6 @@ class exercise2:
             u_reps.append(u_rep)
             u_atts.append(u_att)
             pot_fun = pot_att+pot_repu
-            # print(distance_from_obs_edge)
             q.append(q[iters]-step_size*pot_fun)
 
             iters += 1
@@ -78,30 +77,81 @@ class exercise2:
                 np.array(obs_position)-np.array(position)) - self.length)
             diff = array(obs_position)-array(position)
             edge_point_ang = np.angle(complex(diff[0], diff[1]))
-            edge_point.append([obs_position[0]-self.radius *
-                               np.cos(edge_point_ang), obs_position[1]-self.radius*np.sin(edge_point_ang)])
+            edge_point.append([obs_position[0]-self.radius[i] *
+                               np.cos(edge_point_ang), obs_position[1]-self.radius[i]*np.sin(edge_point_ang)])
         return distance_from_obs_edge, edge_point
 
     def make_obs(self):
         angles = np.linspace(0, 2*pi, 5)
         t_obs = len(self.center)
         ret = []
-        # print(angles)
         for i in range(0, t_obs):
-            bond_obs = [np.transpose(self.radius*np.cos(angles) + self.center[i][0]),
-                        np.transpose(self.radius*np.sin(angles) + self.center[i][1])]
+            bond_obs = [np.transpose(self.radius[i]*np.cos(angles) + self.center[i][0]),
+                        np.transpose(self.radius[i]*np.sin(angles) + self.center[i][1])]
             ret.append(bond_obs)
         return ret
 
+    def make_centers(self):
+        centers = []
+        self.radius = []
+        for i in range(0, len(self.center)):
+            uzip_center = list(zip(*self.center[i]))
+            print(uzip_center)
+            center = [sum(uzip_center[0])/len(uzip_center[0]),
+                      sum(uzip_center[1])/len(uzip_center[1])]
+            radius = np.linalg.norm(array(center)-array(self.center[i][1]))
+            self.radius.append(radius)
+            centers.append(center)
+        self.center = centers
+
     def compute(self):
+        if self.is_coord:
+            self.make_centers()
         q, pot_fun, u_atts, u_reps = self.potential_func()
+        path_len = self.path_length(q)
+        print("Total Path length is: ", path_len)
         q = list(zip(*q))
         return q, pot_fun, u_atts, u_reps
 
+    def potenfield_generator(self):
 
-#  scale = .1
-#         n = 100
-#         iters = 0
-#         step_size = 0.1
-#         d_goal_start = 7
-#         d_q_start = 0.5
+        scale = .1
+        n = 0.1
+        d_goal_start = 1
+        d_q_start = 0.2
+        n_size = 100
+        q1 = np.linspace(-15, 26, n_size)
+        q2 = np.linspace(-15, 26, n_size)
+        U = np.zeros((n_size, n_size))
+        for i in range(0, len(q1)):
+            for j in range(0, len(q1)):
+                qi = [q1[i], q2[j]]
+                distance_from_obs_edge, edge_point = self.get_dis_to_obs(qi)
+                u_att = 0
+                if np.linalg.norm(array(qi)-array(self.goal)) <= d_goal_start:
+                    u_att = .5*scale * \
+                        np.linalg.norm(array(qi)-array(self.goal))
+                else:
+                    u_att = d_goal_start*scale * \
+                        np.linalg.norm(
+                            array(qi)-array(self.goal)) - .5*scale*d_goal_start**2
+                rep = 0
+                u_rep = 0
+                for k in range(0, len(self.center)):
+                    if distance_from_obs_edge[k] <= d_q_start:
+                        rep = .5*n * \
+                            (1/distance_from_obs_edge[k] - 1/d_q_start)**2
+                    else:
+                        rep = 0
+                    u_rep += rep
+                u = u_att+u_rep
+                if u > 2:
+                    u = 2
+                U[i][j] = u
+        return q1, q2, U
+
+    def path_length(self, q):
+        ret = 0
+        for i in range(0, len(q)-1):
+            ret += np.linalg.norm(q[i+1]-q[i])
+        return ret
